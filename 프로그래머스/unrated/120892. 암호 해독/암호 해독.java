ed280d7cd63c816208f6e8389939cedf321c7918
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-22 07:53:31 +0900

    Update 암호 해독.java

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
@@ -9,3 +9,20 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제 풀이 방법
+
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        String[] split = cipher.split("");
+
+        for (int i = 0; i < split.length; i++) {
+            if((i+1)% code ==0){
+                answer += split[i];
+            }
+        }
+
+        return answer;
+    }
+}
