808d75944ad2a523d58542582e5cfcac2d596249
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-22 10:31:05 +0900

    Update 주사위의 개수.java

--- 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.java
@@ -2,9 +2,8 @@ class Solution {
     public int solution(int[] box, int n) {
         int answer = 0;
 
-        // int[] box = [10,8,7]
-        // n => 숫자
-        // 가로 * 세로* 높이 = 부피
+        // int[] box = [10,8,7] 가로 세로 높이 
+        // n => 모서리 길이
         // 가로 세로 높이를 모서리 만큼 나눈다음 가로*세로*높이 해주시면 됩니다
         int row = box[0] / n;
         int colum = box[1] /n;
