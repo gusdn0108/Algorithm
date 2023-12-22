c48f60780b9fc9e1dab89985431de95ba7aa767c
gusdn0108 <96761523+gusdn0108@users.noreply.github.com>
2023-12-22 18:39:39 +0900

    Update 인덱스 바꾸기.java

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -8,9 +8,14 @@ class Solution {
         // "hlelo"
 
         String[] split = my_string.split("");
+        // h,e,l,l,o
         String temp = split[num1];
+        // temp => e
         split[num1] = split[num2];
+        // spilt[num1] 은 e 에서 l 바뀜
         split[num2] = temp;
+        // split[num2] 는 l에서 temp 에서 저장해둔 e 로바뀜
         return String.join("",split);
+        // 이후 split 으로 자른 값을 다시 join("")으로 묶어줌
     }
 }
