bfb3fa4c018d0ea86d9d5cef19e5b41e6414b4dd
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-11 12:50:00 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 232 ms, Memory: 17960 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            String text =  sc.nextLine();
+            System.out.println(text);
+        }
+    }
+}
