26160a650fe06f077732d6fc8158922393d06244
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-01-11 12:47:22 +0900

    [Bronze V] Title: A+B - 8, Time: 284 ms, Memory: 19124 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+
+        for (int i = 1; i < test+1; i++) {
+            int a  = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+i+":"+" "+a+ " + " + b +" = "+(a+b));
+        }
+    }
+}
