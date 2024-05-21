e81d0f121c779b053f9b3f934af60ba050ed127f
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-05-21 13:46:14 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String args[]){
-		
-		Scanner in=new Scanner(System.in);
-				
-		while(true){
-		
-			int A=in.nextInt();
-			int B=in.nextInt();
-		
-			if(A==0 && B==0){
-				in.close();
-				break;
-			}
-			System.out.println(A+B);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+
+        while (true){
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            if(a == 0 && b == 0){
+                break;
+            }
+
+            System.out.println(a+b);
+            
+        }
+
+
+    }
 }
