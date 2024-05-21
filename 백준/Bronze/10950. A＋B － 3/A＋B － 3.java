2563e31db394747ecb103d2950ca55900e9512d9
gusn <96761523+gusdn0108@users.noreply.github.com>
2024-05-21 13:37:31 +0900

    [Bronze V] Title: A+B - 3, Time: 224 ms, Memory: 17940 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,17 +1,17 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
 
+        int n = sc.nextInt();
+        int a,b;
+        for (int i = 0; i < n; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
 
-        for (int i = 0; i < test; i++) {
-            int a =  sc.nextInt();
-            int b = sc.nextInt();
             System.out.println(a+b);
         }
-        
+
     }
 }
