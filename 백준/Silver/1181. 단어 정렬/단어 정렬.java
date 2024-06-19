610170902d59ddfd5891c90938c4a1b4c2a43585
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-06-19 22:09:11 +0900

    [Silver V] Title: 단어 정렬, Time: 916 ms, Memory: 40584 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2);
+                }else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        System.out.println(arr[0]);
+
+        for (int i = 1; i < n; i++) {
+            if(!arr[i].equals(arr[i-1])){
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
