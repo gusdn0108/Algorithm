aa61a823fa3bb2c6de2bd8bad1b25473f38bd892
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-06-20 00:25:38 +0900

    Update 나이순 정렬.java

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine(); // Consume the newline character
+        sc.nextLine(); 
 
         String[][] stringArr = new String[n][2];
 
         for (int i = 0; i < n; i++) {
-            stringArr[i][0] = sc.next(); // Read the age
-            stringArr[i][1] = sc.next(); // Read the name
+            stringArr[i][0] = sc.next(); 
+            stringArr[i][1] = sc.next();
         }
 
         Arrays.sort(stringArr, new Comparator<String[]>() {
