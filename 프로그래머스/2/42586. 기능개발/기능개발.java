7ac4ecedabbdf2becb8d5587d11a3db768c8d838
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-06-21 22:14:15 +0900

    [level 2] Title: 기능개발, Time: 3.63 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
+            }
+            dayOfend[day]++;
+        }
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+    }
+}
