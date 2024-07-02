d77b55ba184857ace14108023181e9e5d0b5bee8
혀누 <96761523+gusdn0108@users.noreply.github.com>
2024-07-02 22:19:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+        
+		int N = in.nextInt();
+		
+		
+		if (N == 4 || N == 7) {
+			System.out.println(-1);
+		}
+		else if (N % 5 == 0) {
+			System.out.println(N / 5);
+		}
+		else if (N % 5 == 1 || N % 5 == 3) {
+			System.out.println((N / 5) + 1);
+		}
+		else if (N % 5 == 2 || N % 5 == 4) {
+			System.out.println((N / 5) + 2);
+		}
+	}
+}
